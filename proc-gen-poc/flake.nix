9f760d43dc4eef904e76fc55893442d9767b9b3e
Milo Moisson <milomoisson@gmail.com>
2024-12-18 01:12:14 +0100

    chore(proc-gen): update flake and cargo lock

--- proc-gen-poc/flake.nix
+++ proc-gen-poc/flake.nix
@@ -1,26 +1,17 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    gitignore = {
-      url = "github:hercules-ci/gitignore.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, gitignore }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
       inherit (nixpkgs.lib) genAttrs;
 
       forAllSystems = genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
       forAllPkgs = function: forAllSystems (system: function pkgs.${system});
-
-      mkApp = (program: { type = "app"; inherit program; });
 
       pkgs = forAllSystems (system: (import nixpkgs {
         inherit system;
@@ -29,15 +20,6 @@
     in
     {
       formatter = forAllPkgs (pkgs: pkgs.nixpkgs-fmt);
-
-      packages = forAllPkgs (pkgs: rec {
-        default = app;
-        app = pkgs.callPackage ./package.nix { inherit gitignore; };
-      });
-      apps = forAllSystems (system: rec {
-        default = app;
-        app = mkApp (pkgs.getExe self.packages.${system}.app);
-      });
 
       devShells = forAllPkgs (pkgs:
         with pkgs.lib;
@@ -52,6 +34,7 @@
               rust-toolchain
               act
             ];
+
             buildInputs = with pkgs; [
               fontconfig
               freetype
@@ -59,8 +42,6 @@
 
             RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
             LD_LIBRARY_PATH = makeLibraryPath buildInputs;
-
-            # RUST_LOG = "";
           };
         });
     };
