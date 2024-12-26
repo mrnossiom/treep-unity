659d4433e3f884bf255bd0342592408e3142fae8
Kristen Couty <kristen.couty@gmail.com>
2024-12-26 19:21:16 +0100

    feat(site-vanilla): landing page draft

---
+++ site-vanilla/main.js
@@ -0,0 +1,12 @@
+console.log("JS loaded");
+
+$(function(){
+  $(window).scroll(function(){
+    var winTop = $(window).scrollTop();
+    if(winTop >= 30){
+      $("body").addClass("sticky-header");
+    }else{
+      $("body").removeClass("sticky-header");
+    }
+  });
+});
