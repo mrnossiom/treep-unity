74382d7f92bd6123dde55aeb50ac03835f943cce
Kristen Couty <kristen.couty@gmail.com>
2025-01-04 19:16:16 +0100

    chore(site-vanilla): remove useless code

--- site-vanilla/ressources/scripts/main.js
+++ site-vanilla/ressources/scripts/main.js
@@ -7,20 +7,3 @@ var numRand = Math.floor(Math.random() * imageNumber);
 var imagePath = `ressources/media/diapo/IMG_${numRand}.png`;
 var bannerElement = document.getElementsByClassName("banner");
 bannerElement[0].style.background = `url(${imagePath}) center/cover no-repeat`;
-
-// OS Detection for download
-function detectOS() {
-  const userAgent = navigator.userAgent;
-  let os = "Unknown OS";
-
-  if (/Windows NT 10.0/.test(userAgent)) os = "Windows 10";
-  else if (/Windows NT 6.3/.test(userAgent)) os = "Windows 8.1";
-  else if (/Windows NT 6.2/.test(userAgent)) os = "Windows 8";
-  else if (/Windows NT 6.1/.test(userAgent)) os = "Windows 7";
-  else if (/Macintosh/.test(userAgent)) os = "macOS";
-  else if (/Linux/.test(userAgent)) os = "Linux";
-  else if (/Android/.test(userAgent)) os = "Android";
-  else if (/iPhone|iPad|iPod/.test(userAgent)) os = "iOS";
-
-  return os;
-}
