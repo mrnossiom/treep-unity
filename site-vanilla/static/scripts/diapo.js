4a9bd077b36428becd381269975b466c9aa17424
Milo Moisson <milomoisson@gmail.com>
2025-01-16 13:40:32 +0100

    fix(site-vanilla): use relative links for github pages

--- site-vanilla/static/scripts/diapo.js
+++ site-vanilla/static/scripts/diapo.js
@@ -1,5 +1,5 @@
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const imagePath = `/static/media/diapo/IMG_${randomNumber(1, 2)}.png`;
+const imagePath = `static/media/diapo/IMG_${randomNumber(1, 2)}.png`;
 const bannerElement = document.getElementsByClassName("banner");
 bannerElement[0].style.background = `url(${imagePath}) center/cover no-repeat`;
