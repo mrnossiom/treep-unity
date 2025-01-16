ebdcab1882d667d6279a9859e0dbb31499fe4e2d
Milo Moisson <milomoisson@gmail.com>
2025-01-16 11:58:54 +0100

    cleanup links

--- site-vanilla/ressources/scripts/main.js
+++
@@ -1,9 +0,0 @@
-console.log("Script loaded")
-
-// Random image
-var imageNumber = 3;
-var numRand = Math.floor(Math.random() * imageNumber);
-
-var imagePath = `ressources/media/diapo/IMG_${numRand}.png`;
-var bannerElement = document.getElementsByClassName("banner");
-bannerElement[0].style.background = `url(${imagePath}) center/cover no-repeat`;
