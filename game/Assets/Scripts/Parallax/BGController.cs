2a956e3d0727ad51555d6e740bab545baedd2819
Romain Dischamp <romain.dischamp@gmail.com>
2025-03-22 16:40:33 +0100

    feat(game): infinite background repeat

--- game/Assets/Scripts/Parallax/BGController.cs
+++ game/Assets/Scripts/Parallax/BGController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 using UnityEngine;
 
 namespace Treep
@@ -5,7 +7,7 @@ namespace Treep
     public class BackgroundController : MonoBehaviour
     {
 
-        private float startPos;
+        private float startPos, length;
         public GameObject cam;
         public float backgroundSpeed; //The intensity of the parallax effect 0 -> 1 (0 move a lot and 1 don't move)
 
@@ -13,13 +15,25 @@ namespace Treep
         void Start()
         {
             startPos = transform.position.x;
+            length = 64; //GetComponent<SpriteRenderer>().size.x is the line to not write magic number, but this line don't work idk why
         }
 
         // Update is called once per frame
         void FixedUpdate()
         {
             float distance = cam.transform.position.x * backgroundSpeed;
+            float movement = cam.transform.position.x * (1 - backgroundSpeed);
+
             transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+            if(movement > startPos + length)
+            {
+                startPos += length;
+            }
+            else if(movement < startPos - length)
+            {
+                startPos -= length;
+            }
         }
     }
 }
