5c74977e82981368a734b8a384aff60dbf59abae
Kristen Couty <kristen.couty@gmail.com>
2025-05-23 20:41:17 +0200

    fix: attack sync

--- game/Assets/Scripts/Player/PlayerCombat.cs
+++ game/Assets/Scripts/Player/PlayerCombat.cs
@@ -1,18 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Mirror;
 using Treep.IA;
 using Treep.SFX;
 using Treep.Weapon;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.Serialization;
 using Debug = UnityEngine.Debug;
 using Random = System.Random;
 using Rect = Treep.Weapon.Rect;
 
 namespace Treep.Player {
-    public class PlayerCombat : MonoBehaviour {
+    public class PlayerCombat : NetworkBehaviour {
         public static Vector2 AttackPointRight = new(1.5f, 0f);
         public static Vector2 AttackPointLeft = new(-1.5f, 0f);
         public static Vector2 AttackPointTop = new(0f, 1.75f);
@@ -22,7 +22,6 @@ namespace Treep.Player {
         public LayerMask enemyLayerMask;
 
         public KeyCode closeAttackKey = KeyCode.L;
-        public KeyCode distAttackKey = KeyCode.O;
 
         private static readonly int IsCloseAttacking = Animator.StringToHash("isCloseAttacking");
         private static readonly int Random = Animator.StringToHash("Random");
@@ -53,58 +52,66 @@ namespace Treep.Player {
         }
 
         public void Update() {
+            if (!this.isLocalPlayer) return;
+
             this.UpdateAttackPoint();
+
             if (Time.time >= this._nextAttackTime) {
                 if (Input.GetKeyDown(this.closeAttackKey)) {
-                    this.audioMixer.GetFloat("SFXVolume", out var soundLevel);
-                    soundLevel = (soundLevel + 80) / 100;
-                    SoundFXManager.Instance.PlaySoundFXClip(this.slashSoundClip, this.transform, soundLevel);
-                    this.CloseAttack();
+                    CloseAttack();
                     this._nextAttackTime = Time.time + 1f / this._weaponManager.AttackRate;
                 }
             }
         }
 
-
         private void UpdateAttackPoint() {
             this._currentLookDirection = this._controllerScript.lookDirection;
             switch (this._currentLookDirection) {
                 case LookDirection.Right:
-                    this.attackPoint.position = PlayerCombat.AttackPointRight + (Vector2)this.transform.position;
+                    this.attackPoint.position = AttackPointRight + (Vector2)this.transform.position;
                     break;
                 case LookDirection.Left:
-                    this.attackPoint.position = PlayerCombat.AttackPointLeft + (Vector2)this.transform.position;
+                    this.attackPoint.position = AttackPointLeft + (Vector2)this.transform.position;
                     break;
                 case LookDirection.Top:
-                    this.attackPoint.position = PlayerCombat.AttackPointTop + (Vector2)this.transform.position;
+                    this.attackPoint.position = AttackPointTop + (Vector2)this.transform.position;
                     break;
                 case LookDirection.Bottom:
-                    this.attackPoint.position = PlayerCombat.AttackPointBottom + (Vector2)this.transform.position;
+                    this.attackPoint.position = AttackPointBottom + (Vector2)this.transform.position;
                     break;
             }
-
 
             this._weaponManager.UpdateLooking(this._currentLookDirection);
         }
 
         private void CloseAttack() {
-            this._animatorController.TriggerAttack(this._currentLookDirection);
+            if (!this.isLocalPlayer) return;
+            
+            this.audioMixer.GetFloat("SFXVolume", out var soundLevel);
+            soundLevel = (soundLevel + 80) / 100;
+            SoundFXManager.Instance.PlaySoundFXClip(this.slashSoundClip, this.transform, soundLevel);
+            
+            CmdPerformAttack(this._controllerScript.lookDirection);
+        }
 
-            this._attackAnimator.SetInteger(PlayerCombat.Random, new Random().Next(6));
-            this._attackAnimator.SetTrigger(PlayerCombat.Attack);
-
-
-            // check enemy
-            var enemiesToHit = this.GetEnemyIn(this._weaponManager.HitBox);
-            // Damage to enemy
+        [Command]
+        private void CmdPerformAttack(LookDirection direction) {
+            RpcPlayAttackAnimation(direction);
+            
+            var enemiesToHit = GetEnemyIn(this._weaponManager.HitBox);
             foreach (var enemy in enemiesToHit) {
                 Debug.Log($"{enemy.name} took {this._weaponManager.Damage} damage");
                 enemy.GetComponent<IEnemy>().Hit(this._weaponManager.Damage);
             }
         }
 
-        private void DistAttack() {
-            this._animatorController.SetTrigger(PlayerCombat.IsDistAttacking);
+        [ClientRpc]
+        private void RpcPlayAttackAnimation(LookDirection direction) {
+            if (!this.isActiveAndEnabled) return;
+
+            this._animatorController.TriggerAttack(direction);
+            this._attackAnimator.SetInteger(Random, new Random().Next(6));
+            this._attackAnimator.SetTrigger(Attack);
         }
 
         private Collider2D[] GetEnemyIn(IShapesHitbox shape) {
@@ -132,25 +139,25 @@ namespace Treep.Player {
 
         private void OnDrawGizmos() {
             Gizmos.color = Color.yellow;
-            Gizmos.DrawSphere((Vector3)PlayerCombat.AttackPointTop + this.transform.position, 0.1f);
-            Gizmos.DrawSphere((Vector3)PlayerCombat.AttackPointBottom + this.transform.position, 0.1f);
-            Gizmos.DrawSphere((Vector3)PlayerCombat.AttackPointRight + this.transform.position, 0.1f);
-            Gizmos.DrawSphere((Vector3)PlayerCombat.AttackPointLeft + this.transform.position, 0.1f);
+            Gizmos.DrawSphere((Vector3)AttackPointTop + this.transform.position, 0.1f);
+            Gizmos.DrawSphere((Vector3)AttackPointBottom + this.transform.position, 0.1f);
+            Gizmos.DrawSphere((Vector3)AttackPointRight + this.transform.position, 0.1f);
+            Gizmos.DrawSphere((Vector3)AttackPointLeft + this.transform.position, 0.1f);
 
             Gizmos.color = Color.black;
 
             switch (this._currentLookDirection) {
                 case LookDirection.Top:
-                    Gizmos.DrawSphere((Vector3)PlayerCombat.AttackPointTop + this.transform.position, 0.1f);
+                    Gizmos.DrawSphere((Vector3)AttackPointTop + this.transform.position, 0.1f);
                     break;
                 case LookDirection.Bottom:
-                    Gizmos.DrawSphere((Vector3)PlayerCombat.AttackPointBottom + this.transform.position, 0.1f);
+                    Gizmos.DrawSphere((Vector3)AttackPointBottom + this.transform.position, 0.1f);
                     break;
                 case LookDirection.Right:
-                    Gizmos.DrawSphere((Vector3)PlayerCombat.AttackPointRight + this.transform.position, 0.1f);
+                    Gizmos.DrawSphere((Vector3)AttackPointRight + this.transform.position, 0.1f);
                     break;
                 case LookDirection.Left:
-                    Gizmos.DrawSphere((Vector3)PlayerCombat.AttackPointLeft + this.transform.position, 0.1f);
+                    Gizmos.DrawSphere((Vector3)AttackPointLeft + this.transform.position, 0.1f);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
