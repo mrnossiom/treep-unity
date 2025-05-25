a308df042d2fbb5f2c42aafd3c87bd82fd6ceb00
Kristen Couty <kristen.couty@gmail.com>
2025-05-25 12:56:21 +0200

    feat: new images for main page diapo

--- site-vanilla/static/scripts/diapo.js
+++ site-vanilla/static/scripts/diapo.js
@@ -1,4 +1,4 @@
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 const bannerElement = document.querySelector(".blured-image");
-bannerElement.src = `static/media/diapo/IMG_${randomNumber(1, 2)}.png`;
+bannerElement.src = `static/media/diapo/IMG_${randomNumber(1, 4)}.png`;
